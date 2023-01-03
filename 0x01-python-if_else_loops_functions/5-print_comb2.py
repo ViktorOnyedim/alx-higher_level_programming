749652eeae10c04cfefa2304195965c5c673035f
ViktorOnyedim <viktoronyedim@gmail.com>
2023-01-03 23:54:35 +0000

    Modified 5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 for i in range(100):
-    if (i < 10):
-        print(f"0{i}", end=", ")
+    print(f"{i:02d}", end=", ")
     if (i == 99):
         print(i)
-    else:
-        print(i, end=", ")

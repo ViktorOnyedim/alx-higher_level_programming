e16d1f566d6b8623367bd7d54f71f7c7f5f72009
ViktorOnyedim <viktoronyedim@gmail.com>
2023-01-06 03:35:20 +0000

    Another quick fix

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,7 +3,6 @@ if __name__ == "__main__":
     import calculator_1 as calc
     a = 10
     b = 5
-    print("__name__ is {}".format(__name__))
     print("{} + {} = {}".format(a, b, calc.add(a, b)))
     print("{} - {} = {}".format(a, b, calc.sub(a, b)))
     print("{} * {} = {}".format(a, b, calc.mul(a, b)))

bb9c4023441610ad8b56d0bff40b652834ca2c38
ViktorOnyedim <viktoronyedim@gmail.com>
2023-01-13 01:39:44 +0000

    Added 0-square_matrix_simple.py

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    rows = len(matrix)
+    cols = len(matrix[0])
+    
+    squared_matrix = []
+    for x in range(rows):
+        row = []
+        for y in range(cols):
+            row.append(0)
+        squared_matrix.append(row)
+
+    for i in range(rows):
+        for j in range(cols):
+            squared_matrix[i][j] = matrix[i][j] ** 2
+    return squared_matrix

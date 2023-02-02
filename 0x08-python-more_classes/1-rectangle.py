7a678355381f6efe985aba119c7e28140b7d2a0d
ViktorOnyedim <viktoronyedim@gmail.com>
2023-02-02 09:17:08 +0000

    Fixed pycodestyle error

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -24,16 +24,16 @@ class Rectangle:
     def width(self, value):
         """Set the width of the rectangle"""
         if not isinstance(value, int):
-            raise TypeError("width must be an integer")       
+            raise TypeError("width must be an integer")
         if (value < 0):
             raise ValueError("width must be >= 0")
         self.__width = value
-    
+
     @property
     def height(self):
         """Retrieve the height of the rectangle"""
         return self.__height
-    
+
     @height.setter
     def height(self, value):
         """Set the height of the rectangle"""

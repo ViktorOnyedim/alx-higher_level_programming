4844c53bb30a1c8c696b78c1fd3e6603e8c8692f
ViktorOnyedim <viktoronyedim@gmail.com>
2023-02-08 11:53:39 +0000

    Added 8-rectangle.py

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -19,16 +19,14 @@ class BaseGeometry:
         This function Validates an integer
 
         Args:
-            name (string): name
+            name (str): The name of the parameter
             value (int): Integer value to be validated
-
-        Returns:
 
         Raises:
             TypeError: if value is not an integer
             ValueError: if value is less or equal to 0
         """
-        if (not isinstance(value, int)):
+        if (type(value) is not int):
             raise TypeError("{} must be an integer".format(name))
         if (value <= 0):
             raise ValueError("{} must be greater than 0".format(name))

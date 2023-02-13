37774904ee9aba0158337061f764837212bb2ad3
ViktorOnyedim <viktoronyedim@gmail.com>
2023-02-13 00:26:10 +0000

    Added 0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This module reads a text file (UTF8)"""
+
+
+def read_file(filename=""):
+    """
+    Reads a text file (UTF8) and prints it to stdout
+    Args:
+        filename: Name of the file to read from
+    """
+    with open(filename, "r", encoding="utf-8") as f:
+        read_data = f.read()
+        print(read_data, end="")
+    f.close()

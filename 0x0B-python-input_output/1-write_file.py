71d02ab326e66c65a7c97412efdd96a5dbc08354
ViktorOnyedim <viktoronyedim@gmail.com>
2023-02-13 00:40:40 +0000

    Added 1-write_file.py

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defines a function that writes a string to a text file"""
+
+
+def write_file(filename="", text=""):
+    """
+    Writes a string to a text file (UTF8)
+    Args:
+        filename: Name of the file to write to
+    Return:
+        Number of characters written
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        write_data = f.write(text)
+        return write_data
+    f.close()        

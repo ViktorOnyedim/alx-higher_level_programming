938e0094f7971af0f2026d26e854f80f05f10916
ViktorOnyedim <viktoronyedim@gmail.com>
2023-02-13 01:04:43 +0000

    Modified 0-read_file.py and 1-write_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -10,5 +10,4 @@ def read_file(filename=""):
     """
     with open(filename, "r", encoding="utf-8") as f:
         read_data = f.read()
-        print(read_data, end="")
-    f.close()
+    print(read_data, end="")
--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -12,5 +12,4 @@ def write_file(filename="", text=""):
     """
     with open(filename, "w", encoding="utf-8") as f:
         write_data = f.write(text)
-        return write_data
-    f.close()
+    return write_data

800c2e2907dce991eec7db6eae8a3345a787c39d
ViktorOnyedim <viktoronyedim@gmail.com>
2023-02-15 22:52:20 +0000

    Updated models/rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -77,3 +77,7 @@ class Rectangle(Base):
             for j in range(self.__width):
                 print("#", end="")
             print()
+
+    def __str__(self):
+        return f"[Rectangle] ({self.id}) \
+{self.__x}/{self.y} - {self.__width}/{self.__height}"

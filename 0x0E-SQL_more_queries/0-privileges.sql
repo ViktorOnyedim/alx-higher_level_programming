4f7b34bd25fdea286d144bb3ea54f50c4c8c0e72
ViktorOnyedim <viktoronyedim@gmail.com>
2023-03-17 04:33:01 +0000

    Modified 0-privileges.sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,2 @@
 -- Lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+SELECT * FROM mysql.user WHERE User IN ('user_0d_1', 'user_0d_2');

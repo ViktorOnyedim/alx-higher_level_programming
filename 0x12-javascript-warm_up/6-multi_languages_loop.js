89b45e540131fd604e7284103558b762808d2f7c
ViktorOnyedim <viktoronyedim@gmail.com>
2023-04-11 15:47:16 +0000

    Added 6-multi_languages_loop.js

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const first = 'C is fun';
+const second = 'Python is cool';
+const third = 'JavaScript is amazing';
+
+const lines = [first, second, third];
+
+for (let i = 0; i < lines.length; i++) {
+  console.log(lines[i]);
+}

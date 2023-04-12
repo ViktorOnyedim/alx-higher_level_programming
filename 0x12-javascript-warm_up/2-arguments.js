3693dae6d300787d4d9fa58ed08e0016994c0048
ViktorOnyedim <viktoronyedim@gmail.com>
2023-04-12 05:47:01 +0000

    Modified 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,13 +1,10 @@
 #!/usr/bin/node
 
-const { argv } = require('node:process');
+const length = process.argv.length;
 
-const args = argv.slice(2);
-const length = args.length;
-
-if (length === 0) {
+if (length < 3) {
   console.log('No argument');
-} else if (length === 1) {
+} else if (length < 4) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');

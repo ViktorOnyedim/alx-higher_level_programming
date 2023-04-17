a19270b5dd1885d629d9e323b879bbf895d387cf
ViktorOnyedim <viktoronyedim@gmail.com>
2023-04-17 13:32:31 +0000

    Modified 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""Lists all states from the database hbtn_0e_0_usa"""
+
 
 import MySQLdb
 from sys import argv
@@ -10,9 +12,7 @@ if __name__ == '__main__':
     """create cursor object to execute queries"""
     cur = db.cursor()
 
-    query = "SELECT * FROM state ORDER BY states.id ASC"
-
-    cur.execute(query)
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
     rows = cur.fetchall()
 
     for row in rows:

cebe4018793287f18f3b3944d42f8e99e7e9792f
ViktorOnyedim <viktoronyedim@gmail.com>
2023-04-18 00:21:28 +0000

    Added 4-cities_by_state.py

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Lists all 'cities' from the database 'hbtn_0e_4_usa'
+Takes 3 arguments:
+-> mysql username
+-> mysql password
+-> database name
+"""
+import MySQLdb
+from sys import argv, exit
+
+
+if __name__ == '__main__':
+    if len(argv) != 4:
+        exit(1)
+
+    """Establish database connection"""
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=argv[1], passwd=argv[2], db=argv[3])
+    """Create cursor object"""
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM cities ORDER BY cities.id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()

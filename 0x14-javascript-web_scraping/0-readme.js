f65956dd9d502a50d034249115cce6cce11c1495
ViktorOnyedim <viktoronyedim@gmail.com>
2023-05-24 12:39:45 +0000

    Added 0-readme.js

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+/* Reads and prints the content of a file */
+
+const fs = require('fs');
+
+if (process.argv.length < 3) {
+  console.error("File name is missing!");
+  process.exit(1);
+}
+
+const fileName = process.argv[2];
+
+fs.readFile(fileName, "utf-8", (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  console.log(data);
+});
